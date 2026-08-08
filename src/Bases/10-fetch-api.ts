6ae36d2c6d8d1e511ea15c3250b7b6146e8a4079
Daniel Orejuela <danielorejuelag@gmail.com>
2026-08-08 13:38:46 -0500

    giphy fetch

--- src/Bases/10-fetch-api.ts
+++ src/Bases/10-fetch-api.ts
@@ -1,15 +1,19 @@
+import type { GiphyRandomResponse } from "../data/giphy.response";
+
 const API_KEY = 'QrTcO5MyIQA5UI4HPkc97DBJKW2CS3KE';
 const myRequest = fetch(`https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}`);
 
-myRequest.then((response)=> response.json())
-.then((data)=> {
-    const imageUrl = data.data.images.original.url;
-    console.log(imageUrl);
-
+const createImageInsideDOM = (url:string) => {
     const imgElement= document.createElement('img')
-    imgElement.src =imageUrl;
+    imgElement.src =url;
 
     document.body.append(imgElement);
+}
+
+myRequest.then((response)=> response.json())
+.then(({data}: GiphyRandomResponse)=> {
+    const imageUrl = data.images.original.url;
+    createImageInsideDOM(imageUrl)
 })
 .catch((error)=>{
     console.error(error)
